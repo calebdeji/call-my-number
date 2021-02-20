03f353069a0614915d358d0e8b99f550dc1a69c6
calebdeji <calebdeji06@gmail.com>
2021-02-20 02:15:47 +0100

    💄 feature: update container style

--- src/components/Container/Container.ts
+++ src/components/Container/Container.ts
@@ -8,6 +8,7 @@ const Container = styled.main`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: rgba(0, 0, 0, 0.05);
 
   @media all and (min-width: 500px) {
     padding: ${convertPixelToRem(10)} 20%;
@@ -20,7 +21,11 @@ export const SubContainer = styled.div`
   row-gap: ${convertPixelToRem(30)};
   min-width: ${convertPixelToRem(250)};
   max-width: ${convertPixelToRem(500)};
-  width: ${convertPixelToRem(300)};
+  width: ${convertPixelToRem(400)};
+  padding: ${convertPixelToRem(30)} ${convertPixelToRem(50)};
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.09);
+  border-radius: ${convertPixelToRem(10)};
+  background-color: rgba(255, 255, 255, 0.3);
 `;
 
 export default Container;
